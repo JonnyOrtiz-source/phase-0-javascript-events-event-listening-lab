4fb5c331a3e6966782e33a7d745e032bb6740705
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-04-01 20:43:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+// add an event listener for the click event on the input#input element in index.html
+const input = document.getElementById('input');
 function addingEventListener() {
+    input.addEventListener('click', () => alert('I was clicked!'));
 }
